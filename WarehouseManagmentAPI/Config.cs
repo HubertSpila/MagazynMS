b5609cd07277f683e6d56db1702c4f80c997e0ee
Hubert Śpila <hubertspila@gmail.com>
2023-01-08 12:28:59 +0100

    cs

--- WarehouseManagmentAPI/Config.cs
+++ WarehouseManagmentAPI/Config.cs
@@ -2,7 +2,8 @@
 {
     public static class Config
     {
-        public static string _connectionString = @"Data Source=.\SQLExpress;Initial Catalog=MagazynMS;Trusted_Connection=True";
+        //public static string _connectionString = @"Data Source=.\SQLExpress;Initial Catalog=MagazynMS;Trusted_Connection=True";
+        public static string _connectionString = @"Data Source=DESKTOP-J9VF8D6\HUBERT;Initial Catalog=MagazynMS;Trusted_Connection=True";
 
     }
 }
