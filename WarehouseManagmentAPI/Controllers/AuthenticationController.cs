e93ccc4e8a597eb2776d4d4b985fd1f410636b72
Hubert <hubertspila@gmail.com>
2023-01-10 12:38:35 +0100

    jwt token

---
+++ WarehouseManagmentAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using WarehouseManagmentAPI.Controllers.PostModels;
+using WarehouseManagmentAPI.Database.DatabaseControllers;
+
+namespace WarehouseManagmentAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private IConfiguration _configuration { get; set; }
+        public AuthenticationController(IConfiguration configuration)
+        {
+            _configuration = configuration?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpPost]
+        public ActionResult<string> Authenticate(AuthenticationPostModel form)
+        {
+            if(!UserDbC.IsOkUser(form)) return Unauthorized();
+
+            var securityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("Authentication:SecretForKey"));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claimsForToken = new List<Claim>();
+            claimsForToken.Add(new Claim("sub", Config.User));
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                _configuration["Authentication:Issuer"],
+                _configuration["Authentication:Audience"],
+                claimsForToken,
+                DateTime.UtcNow,
+                DateTime.UtcNow.AddHours(1),
+                signingCredentials);
+
+            var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+
+            return Ok(tokenToReturn);
+        }
+    }
+}
